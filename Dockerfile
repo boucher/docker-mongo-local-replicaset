61983fa6d2c8a01e51107144c94f3b83bcda1154
Ross Boucher <rboucher@gmail.com>
2016-12-02 11:42:42 -0700

    Use IP directly, rather than localhost. Set mongo into quiet mode.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM mongo:3.0
+FROM mongo:3.0.14
 
 RUN apt-get update && apt-get install -y --no-install-recommends openssl && rm -rf /var/lib/apt/lists/*
 
--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ set -e
 
 USERNAME=${USERNAME:=dev}
 PASSWORD=${PASSWORD:=dev}
+IP=`/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1`
 
 if [ ! "$(ls -A /data/db1)" ]; then
     mkdir /data/db1
@@ -37,9 +38,13 @@ mongod --port 27003 --smallfiles --noprealloc --dbpath /data/db3 --auth --replSe
 DB3_PID=$!
 
 sleep 10
-echo "CONFIGURING REPLICA SET"
-CONFIG='{ _id: "rs0", members: [{_id: 0, host: "localhost:27001", priority: 2 }, { _id: 1, host: "localhost:27002" }, { _id: 2, host: "localhost:27003" } ]}'
+echo "CONFIGURING REPLICA SET: $IP"
+CONFIG="{ _id: 'rs0', members: [{_id: 0, host: '$IP:27001', priority: 2 }, { _id: 1, host: '$IP:27002' }, { _id: 2, host: '$IP:27003' } ]}"
 mongo admin --port 27001 -u $USERNAME -p $PASSWORD --eval "db.runCommand({ replSetInitiate: $CONFIG })"
+
+mongo admin --port 27001 -u $USERNAME -p $PASSWORD --eval "db.runCommand({ setParameter: 1, quiet: 1 })"
+mongo admin --port 27002 -u $USERNAME -p $PASSWORD --eval "db.runCommand({ setParameter: 1, quiet: 1 })"
+mongo admin --port 27003 -u $USERNAME -p $PASSWORD --eval "db.runCommand({ setParameter: 1, quiet: 1 })"
 
 trap 'echo "KILLING"; killall mongod; wait $DB1_PID; wait $DB2_PID; wait $DB3_PID' SIGINT SIGTERM EXIT
 
